9479c36607bade18f9a9e4e65d5bd3bfeb4f3924
Aleksandar43 <a.plahcinski@gmail.com>
2018-02-02 21:01:09 +0100

    Writing syntax errors to log file

--- src/rs/ac/bg/etf/pp1/Compiler.java
+++ src/rs/ac/bg/etf/pp1/Compiler.java
@@ -1,23 +1,32 @@
 /* */
 package rs.ac.bg.etf.pp1;
 
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java_cup.runtime.Symbol;
 import rs.ac.bg.etf.pp1.ast.Program;
 import rs.etf.pp1.symboltable.Tab;
 
 public class Compiler {
+    private static FileWriter logFile;
+    private static PrintWriter printWriter;
     public static void tsdump(){
         Tab.dump();
     }
     public static void main(String args[]){
         try {
             Lexer lexer=new Lexer(new FileReader(args[0]));
+            logFile=new FileWriter("logs/mjlog.txt");
+            printWriter=new PrintWriter(new BufferedWriter(logFile));
             MJParser parser=new MJParser(lexer);
             Symbol rootSymbol = parser.parse();
             Program rootValue = (Program) rootSymbol.value;
-            if(parser.getSyntaxErrors()!=0)
-                System.out.println(parser.getSyntaxErrors()+" syntax error"+(parser.getSyntaxErrors()>1?"s":"")+" detected");
+            if(parser.getSyntaxErrors()!=0){
+                System.out.println(parser.getSyntaxErrors()+" syntax error"+(parser.getSyntaxErrors()>1?"s":"")+" detected\n");
+                printWriter.write("---- Syntax errors ----\n"+parser.getSyntaxErrorMessages());
+            }
             System.out.println("---- Syntax tree ----");
             System.out.println(rootValue.toString(""));
             Tab.init();
@@ -27,6 +36,8 @@ public class Compiler {
             System.out.println(semanticAnalyzer.getSemanticUsageDetections());
             System.err.println(semanticAnalyzer.getSemanticErrors());
             tsdump();
+            printWriter.close();
+            logFile.close();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
